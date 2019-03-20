09d50d48d4963a27208a7d03944a2c77765a5f41
addiani <addiani744@gmail.com>
2019-03-20 18:49:38 -0500

    passed variables

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -5,17 +5,17 @@ properties([parameters([string(defaultValue: '127.0.0.1', description: 'please g
 }
 
     stage("Install git"){
-        sh "ssh  ec2-user@{IP}        sudo yum install git python-pip  -y"
+        sh "ssh  ec2-user@${IP}        sudo yum install git python-pip  -y"
     }
     stage("clone a repo"){
         git 'https://github.com/farrukh90/flask-examples.git'
     }
     stage("Copy files"){
-        sh "scp  *  ec2-user@IP:/TMP/"
+        sh "scp  *  ec2-user@${IP}:/TMP/"
     }    
     stage("Install requirements"){
-        sh "ssh     ec2-user@IP     sudo pip install -r /tmp/requirements.txt"
+        sh "ssh     ec2-user@${IP}     sudo pip install -r /tmp/requirements.txt"
     }
     stage("Run app"){
-        sh 'ssh     ec2-user@IP     python /tmp/01-hello-world/hello-py'
+        sh "ssh     ec2-user@${IP}     python /tmp/01-hello-world/hello-py"
     }
